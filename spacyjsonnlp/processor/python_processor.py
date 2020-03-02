d4e694dfd32adeaebf83f266852648b24d0b99a2
Sander Puts <putssander@gmail.com>
2020-03-02 15:44:52 +0100

    accept json input

--- spacyjsonnlp/processor/python_processor.py
+++ spacyjsonnlp/processor/python_processor.py
@@ -23,41 +23,67 @@ import base64
 from datetime import datetime
 
 from spacyjsonnlp import SpacyPipeline
-from spacyjsonnlp.processor.spacy_to_naf import naf_from_doc, time_in_correct_format, NAF_to_string
+from spacyjsonnlp.processor.spacy_to_naf import time_in_correct_format
 
-from time import sleep, time
+from time import sleep
 
 # print("sleep", flush=True)
 # sleep(60)
 # print("awake", flush=True)
 
+spacy_language = os.environ.get('SPACY_LANGUAGE', 'en')
+spacy_model = os.environ.get('SPACY_MODEL', 'en_core_web_sm')
 
 app = SpacyPipeline()
-app.with_constituents = (os.environ.get('CONSTITUENTS', 'true') != 'false')
-app.with_coreferences = (os.environ.get('COREFERENCES', 'true') != 'false')
-# app.with_dependencies = True
-# app.with_expressions = True
+app.with_constituents = os.environ.get('CONSTITUENTS', 'false')
+app.with_coreferences = os.environ.get('COREFERENCES', 'false')
+app.with_dependencies = os.environ.get('DEPENDENCIES', 'true')
+app.with_expressions = os.environ.get('EXPRESSIONS', 'true')
+
+
+def is_json(unknown):
+    try:
+        json.loads(unknown)
+    except ValueError:
+        return False
+    return True
+
 
 while True:
     for message in consumer:
-        language = 'en'
-        title = ''
-        text = message.value.decode('utf-8')
+        message_value = message.value.decode('utf-8')
+        message_json = None
 
-        logger.debug('In: %s', text)
-        dct_time = time_in_correct_format(datetime.now())
-        start_time = time_in_correct_format(datetime.now())
-        json_nlp, doc = app.process(text)
+        logger.info('In: %s', message_value)
 
-        end_time = time_in_correct_format(datetime.now())
+        try:
 
-        json_nlp_string = json.dumps(json_nlp)
-        naf = get_naf(doc, dct_time, start_time, end_time)
+            dct_time = time_in_correct_format(datetime.now())
+            document_id = dct_time
+            start_time = time_in_correct_format(datetime.now())
 
-        logger.debug(naf.decode('utf-8'))
-        naf_encoded = base64.b64encode(naf).decode('utf-8')
+            if is_json(message_value):
+                message_json = json.loads(message_value)
+                message_json['spacy_model'] = spacy_model
+            if 'identifier' in message_value:
+                document_id = message_json['identifier']
+                json_nlp, doc = app.process(message_json)
+            else:
+                json_nlp, doc = app.process(text=message_value, spacy_model=spacy_model)
+
+            end_time = time_in_correct_format(datetime.now())
+
+            json_nlp_string = json.dumps(json_nlp)
+            naf = get_naf(doc, dct_time, start_time, end_time, spacy_model, spacy_language, message_json)
+
+            logger.debug(naf.decode('utf-8'))
+            naf_encoded = base64.b64encode(naf).decode('utf-8')
+
+        except Exception as e:
+            logger.error("except", e)
 
         json_result = json.dumps({
+            'name': document_id,
             'json-nlp': json.dumps(json_nlp),
             'naf-base64': naf_encoded
         })
--- spacyjsonnlp/processor/util/naf_util.py
+++ spacyjsonnlp/processor/util/naf_util.py
@@ -1,11 +1,19 @@
 from spacyjsonnlp.processor.spacy_to_naf import naf_from_doc, NAF_to_string
 
 
-def get_naf(doc, dct_time, start_time, end_time):
-    model_name = 'model'
+def get_naf(doc, dct_time, start_time, end_time, spacy_model, spacy_language, json_message):
+    model_name = spacy_model
     model_version = 'model-version'
     layer_to_attributes_to_ignore = dict()
     map_udpos2naf_pos = False
+
+    title = uri = ''
+
+    if json_message:
+        if 'title' in json_message:
+            title = json_message['title']
+        if 'uri' in json_message:
+            uri = json_message['uri']
 
     naf = naf_from_doc(doc=doc,
                        dct=dct_time,
@@ -13,9 +21,9 @@ def get_naf(doc, dct_time, start_time, end_time):
                        end_time=end_time,
                        modelname=model_name,
                        modelversion=model_version,
-                       language='en',
-                       title='',
-                       uri='',
+                       language=spacy_language,
+                       title=title,
+                       uri=uri,
                        layers={'raw',
                                'text',
                                'terms',
